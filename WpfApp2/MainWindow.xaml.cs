3d00581b54d4918016aff607e799ef0aa340c26b
unknown <annouri.ilias@gmail.com>
2017-10-06 16:30:05 +0200

    Login Desing Improved

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WpfApp2
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
